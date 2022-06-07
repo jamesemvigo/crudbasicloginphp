8b4447db1e25dee6ec1f404d37dcd68ae6c9cf19
jamesuidev <jamesbaby774@gmail.com>
2022-06-07 19:40:34 +0530

    final validation done

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const formWrapper = document.querySelector(".form-wrapper");
 const actualForm = document.querySelector(".actual-form");
 const inputFields = document.querySelectorAll("input");
 const numberField = document.querySelector("#ph-number");
-const emailField = document.querySelector("#ph-number");
+const emailField = document.querySelector("#email-id");
 const activedashBtn = document.querySelector(".card-button");
 const closeBtn = document.querySelector(".closebody");
 const cancelBtn = document.querySelector(".cancel");
 const activateBtn = document.querySelector(".activate");
+const numberError = document.querySelector(".number-error");
+const emailError = document.querySelector(".email-error");
 let inputNumber;
 let inputEmail;
 activedashBtn.addEventListener("click", (e) => {
@@ -16,7 +18,7 @@ activedashBtn.addEventListener("click", (e) => {
   actualForm.classList.add("active");
 });
 closeBtn.addEventListener("click", (e) => {
-  console.log(e);
+  e.preventDefault();
   removeformSlide();
 });
 cancelBtn.addEventListener("click", (e) => {
@@ -29,6 +31,7 @@ activateBtn.addEventListener("click", (e) => {
 });
 numberField.addEventListener("input", updateNumberInput);
 emailField.addEventListener("input", updateEmailInput);
+
 // numberField.addEventListener("ends",()=>{
 //if(inputNumber.function(0,3)=== "+91" && inputNumber.lenght === 13){
 //  const numberCheckContainer=document.createElement("div");
@@ -44,9 +47,23 @@ emailField.addEventListener("input", updateEmailInput);
 
 function updateNumberInput(e) {
   inputNumber = e.target.value;
+  console.log(inputNumber);
+  let phoneno = /^\+?([0-9]{2})\)?[-. ]?([0-9]{5})[-. ]?([0-9]{5})$/;
+  if (!inputNumber.match(phoneno)) {
+    numberError.innerHTML = `<i class="fa-solid fa-thumbs-down red"></i>`;
+  } else {
+    numberError.innerHTML = `<i class="fa-solid fa-thumbs-up green"></i>`;
+  }
 }
 function updateEmailInput(e) {
   inputEmail = e.target.value;
+  console.log(inputEmail);
+  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!inputEmail.match(mailformat)) {
+    emailError.innerHTML = `<i class="fa-solid fa-thumbs-down red"></i>`;
+  } else {
+    emailError.innerHTML = `<i class="fa-solid fa-thumbs-up green"></i>`;
+  }
 }
 function removeformSlide() {
   setTimeout(() => {
@@ -55,5 +72,7 @@ function removeformSlide() {
   inputFields.forEach((input) => {
     input.value = "";
   });
+  emailError.innerHTML = "";
+  numberError.innerHTML = "";
   actualForm.classList.remove("active");
 }
